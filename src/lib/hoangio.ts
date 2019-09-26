8867a5e7af38df64d653b4e82b7def32b72d5702
Carlos Alba <soradev4@gmail.com>
2019-09-26 14:33:44 -0700

    Deletion of old function

--- src/lib/hoangio.ts
+++ src/lib/hoangio.ts
@@ -4,42 +4,6 @@
  * @param school - Name of School
  * @param data - JSON data
  */
-function HoangioOverlay(name: String, school: String, data: any): HTMLElement {
-    const container = document.createElement("div")
-    container.setAttribute("class", "hoangio-overlay")
-    container.style.display = "block"
-    const button = document.createElement("button");
-    button.innerText = "x";
-    button.addEventListener('click', (_) => {
-        container.style.display = "none";
-    });
-    container.appendChild(button)
-    return container;
-}
-
-/**
- * 
- * @param name - Name of professor
- * @param school - Name of School
- */
-export function HoangioButton(name: String, school: String, sibling: Element): void {
-    const button = document.createElement("button");
-    button.setAttribute("class", "hoangio-button")
-    button.innerText = "Hoangio";
-    button.addEventListener('click', (_) => {
-        const overlay = HoangioOverlay(name, school, {});
-        sibling.parentNode.appendChild(overlay);
-    })
-    sibling.parentNode.appendChild(button);
-}
-
-
-/**
- * 
- * @param name - Name of Professor
- * @param school - Name of School
- * @param data - JSON data
- */
 const OverlayFactory = (name: String, school: String, data: any): any => ({
     render: (): HTMLElement => {
         const container = document.createElement("div")
