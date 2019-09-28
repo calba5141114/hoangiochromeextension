1e00f21532fe48e643eb70b4107487670577b6cf
Carlos Alba <soradev4@gmail.com>
2019-09-27 18:59:47 -0700

    styling functionality

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,29 +1,21 @@
 import { HoangioComponentFactory } from './lib/hoangio';
 
-if (window.location.href.includes("deanza.edu")) {
-    try {
-        document
+// ternary for de anza
+(window.location.href.includes("deanza.edu")) ? (() => {
+    document
             .querySelectorAll(`a[href*="/directory/user.html?u="]`)
             .forEach((el, key) => {
                 const { render } = HoangioComponentFactory(el.textContent, "De Anza College", el);
                 render();
             })
-    } catch (error) {
-        console.error(error);
-        alert("Hoangio failed to render.")
-    }
-}
-else if (window.location.href.includes("foothill.edu")) {
-    try {
+})() : null;
+
+// ternary for foothill
+(window.location.href.includes("foothill.edu")) ? (() => {
         document
             .querySelectorAll(`a[href*="/directory/directory.html?name="]`)
             .forEach((el, key) => {
                 const { render } = HoangioComponentFactory(el.textContent, "Foothill College", el);
                 render();
             });
-    }
-    catch (error) {
-        console.error(error);
-        alert("Hoangio failed to render.")
-    }
-}
+})() : null;
--- src/lib/hoangio.ts
+++ src/lib/hoangio.ts
@@ -1,18 +1,19 @@
+import * as Hammer from 'hammerjs';
+
 /**
  * 
  * @param name - Name of Professor
  * @param school - Name of School
  * @param data - JSON data
  */
-const OverlayFactory = (name: String, school: String, data: any): any => ({
+const OverlayFactory = (name: String, school: String, data: any, sibling: Element): any => ({
     render: (): HTMLElement => {
         const container = document.createElement("div")
         container.setAttribute("class", "hoangio-overlay")
         container.style.display = "block"
-        const button = document.createElement("button");
-        button.innerText = "x";
-        button.addEventListener('click', (_) => container.style.display = "none");
-        container.appendChild(button)
+        // Hammer JS touch functionality 
+        const hammer = new Hammer(container);
+        hammer.on('tap', ev => container.style.display = "none");
         return container;
     }
 });
@@ -27,7 +28,7 @@ export const HoangioComponentFactory = (name: string, school: String, sibling: E
         const button = document.createElement("button");
         button.setAttribute("class", "hoangio-button")
         button.innerText = "Hoangio";
-        const overlay = OverlayFactory(name, school, {});
+        const overlay = OverlayFactory(name, school, {}, sibling);
         button.addEventListener('click', (_) => sibling.parentNode.appendChild(overlay.render()))
         sibling.parentNode.appendChild(button);
     }
