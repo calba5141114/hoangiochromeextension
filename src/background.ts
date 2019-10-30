f532ccb265c78563873dfc3bd5c33a14b48b583d
Carlos Alba <soradev4@gmail.com>
2019-10-29 18:55:46 -0700

    networking

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,6 @@
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
-      if (request.message === "hi")
-        sendResponse({message: "hi to you"});
+        if (request.networking)
+            sendResponse({ message: "hi to you" });
     });
+
