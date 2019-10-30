77874e85f32383bb87dc29daa352885093f3a0b8
Carlos Alba <soradev4@gmail.com>
2019-10-29 18:59:37 -0700

    change

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,14 @@
 
 const handleNetworking = async (request: any) => {
-
+    
 }
 
-chrome.runtime.onMessage.addListener( async (request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     if (request.networking) {
-        sendResponse({result: await handleNetworking(request)})
+        sendResponse({ result: await handleNetworking(request) })
     }
     else {
-        sendResponse({message: "unknown request and or command."})
+        sendResponse({ message: "unknown request and or command." })
     }
 });
 
