a19b9c9128d2351e5ce8e098c80396a0ae928359
Carlos Alba <soradev4@gmail.com>
2019-10-29 18:45:48 -0700

    restruct

--- src/lib/hoangio.ts
+++ src/lib/hoangio.ts
@@ -1,23 +1,27 @@
 import * as Hammer from 'hammerjs';
 
+const SOLRQueryStringGenerator = (name: String, school: String) => `https://search-production.ratemyprofessors.com/solr/rmp/select/?solrformat=true&rows=2000&wt=json&json.wrf=noCB&callback=noCB&q=${name}&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+teacherfullname_autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=score+desc&defType=edismax&siteName=rmp&rows=2000&group=off&group.field=content_type_s&group.limit=2000&fq=schoolname_t%3A%22De+Anza+College%22`;
+
 /**
  * 
  * @param name - Name of Professor
  * @param school - Name of School
  * @param data - JSON data
  */
-const OverlayFactory = (name: String, school: String, data: any, sibling: Element): any => ({
-    render: (): HTMLElement => {
-        console.log(name, school, data);
-        const container = document.createElement("div")
-        container.setAttribute("class", "hoangio-overlay")
-        container.style.display = "block"
-        // Hammer JS touch functionality 
-        const hammer = new Hammer(container);
-        hammer.on('tap', ev => container.style.display = "none");
-        return container;
+const OverlayFactory = (name: String, school: String, data: any, sibling: Element): any => {
+    return {
+        render: (): HTMLElement => {
+            console.log(name, school, data);
+            const container = document.createElement("div")
+            container.setAttribute("class", "hoangio-overlay")
+            container.style.display = "block"
+            // Hammer JS touch functionality 
+            const hammer = new Hammer(container);
+            hammer.on('tap', ev => container.style.display = "none");
+            return container;
+        }
     }
-});
+};
 
 /**
  * 
